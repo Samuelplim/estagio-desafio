a4c4c3b4b8f95088723f70c9ef472c88077743a9
samuelplim <samuel12nerd@gmail.com>
2023-09-10 23:19:27 -0400

    user interface

--- back-end/src/users/dto/create-user.dto.ts
+++ back-end/src/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsNotEmpty, Length } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @Length(8, 16)
+  password: string;
+}
