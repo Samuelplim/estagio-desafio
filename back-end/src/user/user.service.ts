228306a0ec3da5c6ba66b5e11789bf37d53e4630
samuelplim <samuel12nerd@gmail.com>
2023-09-12 19:13:51 -0400

    [feat] service class user

--- back-end/src/user/user.service.ts
+++ back-end/src/user/user.service.ts
@@ -1,20 +1,44 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, Res } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
+import { PrismaService } from "src/prisma/prisma.service";
+import { Prisma, User } from "@prisma/client";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return;
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return await this.prisma.user.create({
+      data: {
+        email: createUserDto.email,
+        password: createUserDto.password,
+        name: createUserDto.name,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return;
+  async findOne(id) {
+    const user = await this.prisma.user.findFirst({ where: { id } });
+    if (!user) {
+      throw new HttpException("User não encontrado", HttpStatus.FORBIDDEN);
+    }
+    return user;
   }
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return;
+
+  async update(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { data, where } = params;
+
+    return await this.prisma.user.update({ where, data });
   }
-  remove(id: number) {
-    return;
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    const response = await this.prisma.user.delete({ where: { id } });
+    return response;
   }
 }
