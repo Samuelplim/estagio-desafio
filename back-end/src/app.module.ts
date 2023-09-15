e43a062fde9ffa29f68c7dc96a535217f30ed7af
samuelplim <samuel12nerd@gmail.com>
2023-09-14 21:12:28 -0400

    [feat] add Auth App

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,3 +1,4 @@
+import { APP_GUARD } from "@nestjs/core";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
@@ -7,11 +8,23 @@ import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
 
 import { UserModule } from "./user/user.module";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
+import { JwtAuthGuard } from "./auth/guards/jwt.guard";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
